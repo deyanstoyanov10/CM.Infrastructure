32585cb0211448170da9f9e471acda90a7c72320
Deyan Stoyanov <didko985@gmail.com>
2022-09-29 21:29:33 +0300

    Add healthcheck extensions

---
+++ src/CM.Infrastructure/CM.Infrastructure/HealthCheck/HealthCheckExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace CM.Infrastructure.HealthCheck
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+    using System;
+    using System.Linq;
+    using System.Net.Mime;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using System.Text.Json.Serialization;
+
+    public static class HealthCheckExtensions
+    {
+        public static IEndpointRouteBuilder MapServiceHealthCheck(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapHealthChecks("/healthcheck", new HealthCheckOptions()
+            {
+                ResponseWriter = WriteResponse
+            });
+
+            return endpoints;
+        }
+
+        internal static Task WriteResponse(
+        HttpContext context,
+        HealthReport report)
+        {
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = false,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
+
+            string json = JsonSerializer.Serialize(
+                new
+                {
+                    Status = report.Status.ToString(),
+                    Duration = report.TotalDuration,
+                    Info = report.Entries
+                        .Select(e =>
+                            new
+                            {
+                                Key = e.Key,
+                                Description = e.Value.Description,
+                                Duration = e.Value.Duration,
+                                Status = Enum.GetName(
+                                    typeof(HealthStatus),
+                                    e.Value.Status),
+                                Error = e.Value.Exception?.Message,
+                                Data = e.Value.Data
+                            })
+                        .ToList()
+                },
+                jsonSerializerOptions);
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
