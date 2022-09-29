4fb854dcdb83405e56049fa4f751f5bfb2925864
Deyan Stoyanov <didko985@gmail.com>
2022-09-29 21:34:31 +0300

    Configure application extensions

--- src/CM.Infrastructure/CM.Infrastructure/ApplicationExtensions.cs
+++ src/CM.Infrastructure/CM.Infrastructure/ApplicationExtensions.cs
@@ -1,6 +1,35 @@
 ﻿namespace CM.Infrastructure
 {
+    using Infrastructure.Consul;
+    using Infrastructure.Logging;
+    using Infrastructure.HealthCheck;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+
     public static class ApplicationExtensions
     {
+        public static WebApplicationBuilder RegisterCMI(this WebApplicationBuilder builder)
+        {
+            builder.Services
+                            .RegisterConsul(builder.Configuration)
+                            .AddHealthChecks();
+
+            builder.Host
+                        .ConfigureLogging(builder.Configuration)
+                        .RegisterConsulConfigurationProvider();
+
+            return builder;
+        }
+
+        public static WebApplication UseCMI(this WebApplication app)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapServiceHealthCheck();
+            });
+
+            return app;
+        }
     }
 }
